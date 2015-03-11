7a18788500bc5f693f36b9cd60f82f86fac2370b
Sharwan Joram <sjoram@jiffynow.in>
2015-03-11 12:20:06 +0000

    Changed the jiffynow.in to http://jiffynow.in in config file

--- modules/utils/config.py
+++ modules/utils/config.py
@@ -2,7 +2,7 @@ import logging as logs
 
 JIFFY_SUPPORT_EMAIL = "connect@jiffynow.in"
 JIFFY_SUPPORT_EMAIL_PWD = "jiffy_!@34"
-JIFFY_WEBSITE = "JiffyNow.in"
+JIFFY_WEBSITE = "http://JiffyNow.in"
 JIFFY_EMAIL_SERVER = "smtp.zoho.com"
 JIFFY_EMAIL_PORT = 465
 JIFFY_SUPPORT_TEAM_1 = 'bpant@jiffynow.in'
