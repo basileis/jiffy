dc2404058da1becfe7c98279acaf4dfad3d27cf8
Sharwan Joram <sjoram@jiffynow.in>
2015-03-11 11:53:17 +0000

    Fixed a bug

--- modules/utils/common.py
+++ modules/utils/common.py
@@ -4,7 +4,7 @@ def get_sha224_hex_digest(string):
     """Returns the hexdigest of the string"""
     h = hashlib.new('sha224')
     h.update(string)
-    print h.hexdigest()
+    return h.hexdigest()
 
 if __name__ == '__main__':
     print get_sha224_hex_digest('bhupesh is the dev')
